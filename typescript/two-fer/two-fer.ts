dd899bfe3761fe664c33d109f436e587c446f49a
Ngalim Siregar <ngalim.siregar@gmail.com>
2022-02-11 13:28:21 +0700

    [#2] Typescript Two Fer Problem

---
+++ typescript/two-fer/two-fer.ts
@@ -0,0 +1,9 @@
+/**
+ * This stub is provided to make it straightforward to get started.
+ */
+
+export function twoFer(name: string = "you"): string {
+  let msg: string = `One for ${name}, one for me.`;
+
+  return msg
+}
